fd9e4173edb2a74f3384d6eadadb0193657d57a9
Yanjare <yanjaregowd1213@gmial.com>
2024-09-18 01:52:40 +0530

    kafka

---
+++ User Service_3/src/main/java/org/training/user/service/model/entity/UserCreatedEvent.java
@@ -0,0 +1,15 @@
+package org.training.user.service.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreatedEvent {
+    private Long userId;
+    private String emailId;
+    private String fullName;
+    private String status;
+}
